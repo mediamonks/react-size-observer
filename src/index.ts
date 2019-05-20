e6ef7a612a98022aa4748570a7f6a344dd397d0a
Floris Bernard <floris.bernard@gmail.com>
2019-05-20 17:28:03 +0200

    Rename factory function to create
    This is to fix the casing of SizeObserver

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ export * from './lib/types';
 export { default as createSizesConfig } from './lib/createSizesConfig';
 export { default as DefaultSizeObserverContext } from './lib/DefaultContext';
 
+import createSizeObserverComponent from './lib/createSizeObserverComponent';
 import defaultPackageConfig from './lib/defaultPackageConfig';
-import sizeObserverFactory from './lib/sizeObserverFactory';
-const SizeObserver = sizeObserverFactory(defaultPackageConfig);
+
+const SizeObserver = createSizeObserverComponent(defaultPackageConfig);
+
 export default SizeObserver;
